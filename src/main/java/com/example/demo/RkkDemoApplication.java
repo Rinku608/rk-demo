96213cedd938c9e76f251f7a2a03d716e7a807b0
Rinku608 <rinku.kumari@mavenwave.com>
2022-12-05 16:04:58 +0530

    initial commit

---
+++ src/main/java/com/example/demo/RkkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RkkDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/Student.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Student {
+	
+	
+@RequestMapping("/")
+	   public String hello() {
+	        return "Hello student, Spring Boot!";
+	    }
+
+@RequestMapping("/student")
+public String welcome() {
+     return "Welcome student in spring boot world!";
+ }
+
+}
